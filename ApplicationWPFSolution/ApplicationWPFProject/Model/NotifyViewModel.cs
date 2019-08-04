72abb4d73c73866fc88724c69456baea9cddc300
Zoltan Herczeg <herczeg.zoltan92@gmail.com>
2019-08-04 19:29:24 +0200

    Basic template for create WPF MVVM application fast

---
+++ ApplicationWPFSolution/ApplicationWPFProject/Model/NotifyViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace ApplicationWPFProject.Model
+{
+    public class NotifyViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ApplicationWPFSolution/ApplicationWPFProject/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ApplicationWPFProject.Model;
+using ApplicationWPFProject.Infrastructure;
+using System.Windows;
+namespace ApplicationWPFProject.ViewModel
+{
+    public class MainViewModel : NotifyViewModel
+    {
+        private ICommand m_sendCommand;
+        private string m_customText;
+
+        public ICommand CmdSend => m_sendCommand;
+
+
+        public MainViewModel()
+        {
+            m_sendCommand = new DelegateCommand(Send);
+        }
+
+        private void Send()
+        {
+            CustomText = "1000";
+        }
+        public string CustomText
+        {
+            get
+            {
+                return m_customText;
+            }
+            set
+            {
+                m_customText = value;
+                OnPropertyChanged("CustomText");
+
+            }
+        }
+
+    }
+}
